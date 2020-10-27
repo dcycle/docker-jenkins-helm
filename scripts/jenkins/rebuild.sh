0429dbdb316319634a133703714202d57e1ced25
alberto56 <albert@mediatribe.net>
2020-10-27 09:22:56 -0400

    Add jenkins script

---
+++ scripts/jenkins/rebuild.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+source ~/.docker-host-ssh-credentials
+
+# Create a droplet
+DROPLET_NAME=docker-jenkins-helm
+PRIVATE_IP=$(ssh "$DOCKERHOSTUSER"@"$DOCKERHOST" \
+  "./digitalocean/scripts/new-droplet.sh $DROPLET_NAME")
+# https://github.com/dcycle/docker-digitalocean-php#public-vs-private-ip-addresses
+IP=$(ssh "$DOCKERHOSTUSER"@"$DOCKERHOST" "./digitalocean/scripts/list-droplets.sh" |grep "$PRIVATE_IP" --after-context=10|tail -1|cut -b 44-)
+echo "Created Droplet at $IP"
+sleep 90
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \
+  root@"$IP" "mkdir -p docker-jenkins-helm-job"
+scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \
+  ~/.dcycle-docker-credentials.sh \
+  root@$IP:~/.dcycle-docker-credentials.sh
+scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \
+  -r * root@"$IP":docker-jenkins-helm-job
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \
+  root@"$IP" "cd docker-jenkins-helm-job && ls -lah && ./scripts/rebuild.sh"
